ba573b3d0659808be5d12d538fabc43ecc0cf0ee
AnvilDev <andres.ernesto.valdes@gmail.com>
2018-07-13 01:44:28 -0400

    Squashed commits

--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,9 @@
+# Set env vars
 export IMAGE_NAME=mangohacks2019-api
-
-# Extract information for tagging
 export COMMIT=${TRAVIS_COMMIT::8}
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 
+# Docker build and tag image
 docker build -t ${IMAGE_NAME}:${BRANCH}-${COMMIT} .
+docker tag ${IMAGE_NAME}:${BRANCH}-${COMMIT} ${HOST_NAME}/${PROJECT_ID}/${IMAGE_NAME}:${BRANCH}-${COMMIT}
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,9 @@
+# Set env vars
+export IMAGE_NAME=mangohacks2019-api
+export COMMIT=${TRAVIS_COMMIT::8}
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+
 # Decrypt our keys
 openssl aes-256-cbc -K $encrypted_f24c28559e81_key -iv $encrypted_f24c28559e81_iv -in client-secret.json.enc -out client-secret.json -d
 
@@ -9,19 +15,19 @@ if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; ex
 source /home/travis/google-cloud-sdk/path.bash.inc
 gcloud version
 
-tar -xzf credentials.tar.gz
-mkdir -p lib
-
 # Here we use the decrypted service account credentials to authenticate the command line tool.
 gcloud auth activate-service-account --key-file client-secret.json
 
-# Install Kubernetes.
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl && \
-    chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-
 # Push to Docker registry.
-gcloud docker -- push ${HOST_NAME}/${PROJECT_ID}/${IMAGE_NAME}:${COMMIT}
+gcloud docker -- push ${HOST_NAME}/${PROJECT_ID}/${IMAGE_NAME}:${BRANCH}-${COMMIT}
 
-# Run on Kubernetes.
-kubectl run mangohacks2019-api --image=${HOST_NAME}/${PROJECT_ID}/${IMAGE_NAME}:${BRANCH}-${COMMIT} --port 9000
-kubectl expose deployment mangohacks2019-api --type=LoadBalancer --port 80 --target-port 9000
+# Install Kubernetes.
+gcloud components install kubectl
+
+# Decrypt and set Kubeconfig
+openssl aes-256-cbc -K $encrypted_f24c28559e81_key -iv $encrypted_f24c28559e81_iv -in config.enc -out config -d
+mkdir $HOME/.kube
+mv config $HOME/.kube/config
+
+# Update on Kubernetes.
+kubectl set image deployment/mangohacks2019-api mangohacks2019-api=${HOST_NAME}/${PROJECT_ID}/${IMAGE_NAME}:${BRANCH}-${COMMIT}
