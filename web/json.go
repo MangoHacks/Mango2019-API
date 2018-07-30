12aa289b5f50e99803bcfaa355dbcc97a6b18f12
Andres Valdes <andres.ernesto.valdes@gmail.com>
2018-07-30 13:25:15 -0400

    Update json.go

--- web/json.go
+++ web/json.go
@@ -9,11 +9,11 @@ import (
 
 // ReadJSONBodyIntoStruct will take the given io.ReadCloser of a JSON and read it into a matching, given struct.
 func ReadJSONBodyIntoStruct(body io.ReadCloser, v interface{}) *HTTPError {
-	defer body.Close()
 	b, err := ioutil.ReadAll(body)
 	if err != nil {
 		return &BadRequestError
 	}
+	defer body.Close()
 	log.Printf("received the following payload: %s", string(b))
 	if err := json.Unmarshal(b, &v); err != nil {
 		return &BadRequestError
