c5672d603ea92938aceb089adad34224dba59633
AnvilDev <andres.ernesto.valdes@gmail.com>
2018-08-21 22:27:48 -0400

    deprecate use of latest tag

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -4,8 +4,4 @@ export COMMIT=${TRAVIS_COMMIT::8}
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 
-if [ "${BRANCH}" == "master" ]; then
-    export TAG=latest
-else
-    export TAG=${BRANCH}-${COMMIT}
-fi
+export TAG=${BRANCH}-${COMMIT}
